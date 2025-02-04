bf932780620f5a819452d3edde58a0989bbd954f
lily <lily.you@student.tdsb.on.ca>
2025-02-04 20:10:11 +0000

    Completed Review 1

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,43 @@
 public class Review1 {
 
+
+	public static void main(String args[]) {
+		q1();
+		q2();
+		q3();
+		q4();
+		q5();
+	}
+
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int firstNum = 9;
+		int secondNum = 5;
+		System.out.println(firstNum/secondNum);
+		System.out.println(firstNum%secondNum);
 	}
-
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		boolean bool = true;
+		System.out.println(bool);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String firstWord = "Hello";
+		String secondWord = "World";
+		System.out.print(firstWord);
+		System.out.println(" " + secondWord);
 	}
 
 }
